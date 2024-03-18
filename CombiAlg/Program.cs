73787694ee1d9ea2056b72c573a89a4fcd3553bd
mrchysen <mrchysen@mail.ru>
2024-03-18 19:16:31 +0700

    upd: write printing for 1 task

--- CombiAlg/Program.cs
+++ CombiAlg/Program.cs
@@ -1,9 +1,38 @@
 ﻿using MathAlg2;
+using MathAlg2.TimeSpanExtension;
+using Spectre.Console;
+using System.Text;
 
-var a = CombinativeGenerator.GenerateCombinations(5,2);
-var b = CombinativeGenerator.GeneratePermutations(3);
+#region реализация алгоритмов 3.1-3.2
+DateTime d = DateTime.Now;
+int N = 8;
+int M = 3;
+var a = CombinativeGenerator.GenerateCombinations(N, M);
+string timeFirstAlg = (DateTime.Now - d).ToStringCustom();
+Print("[red]Алгоритм 3.1[/] Сочетания", a, timeFirstAlg, $"[aqua]N = {N}, M = {M}[/]");
 
-Console.WriteLine("--- Комбинации ---");
-Console.WriteLine(string.Join($"\n", a));
-Console.WriteLine("--- Перестановки ---");
-Console.WriteLine(string.Join($"\n", b));
+d = DateTime.Now;
+N = 6;
+var b = CombinativeGenerator.GeneratePermutations(N);
+timeFirstAlg = (DateTime.Now - d).ToStringCustom();
+//Console.WriteLine(string.Join("\n", b));
+Print("[red]Алгоритм 3.2[/] Перестановки", b,timeFirstAlg, $"[aqua]N = {N}[/]");
+#endregion
+
+
+static void Print(string titleName, List<string> data, string timeSpan, string addInfo)
+{
+    string additionInfo = $"[aqua]время {timeSpan}[/]" + '\n' +
+         addInfo + '\n' +
+         string.Join($"\n", data);
+    var panel = new Panel(new Markup(additionInfo))
+    {
+        Expand = true,
+        Header = new(titleName),
+        Border = BoxBorder.Ascii
+    };
+    AnsiConsole.Write(panel);
+    Console.ReadKey();
+    Console.Clear();
+}
+
---
+++ MathAlg/TimeSpanExtension/TimeSpanExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace MathAlg2.TimeSpanExtension;
+
+public static class TimeSpanExtension
+{
+    public static string ToStringCustom(this TimeSpan t)
+    {
+        int millis = t.Milliseconds;
+        int sec = t.Seconds;
+        int min = t.Minutes;
+
+        return $"<mm:ss:msms>{min}:{sec}:{millis}";
+    }
+}
